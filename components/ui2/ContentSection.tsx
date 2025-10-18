71cf7bf391dc22f11272a0f4e28ef9d05cc575fd
Chanraksa Ory <OCraksa@gmail.com>
2025-10-18 21:14:28 +0700

    fix: border-radius abit mroe

--- components/ui2/ContentSection.tsx
+++ components/ui2/ContentSection.tsx
@@ -11,7 +11,7 @@ export default function ContentSection({ text, src, order }: ContentSectionProps
                 <div className="text-center px-4 my-20 lg:my-0">
                     <p className="text-6xl font-bold ">{text}</p>
                 </div>
-                <div className="relative group max-w-sm sm:max-w-md  rounded-[60px] hover:scale-[1.03] hover:rotate-2  transition-all duration-500 p-14">
+                <div className="relative group max-w-sm sm:max-w-md  rounded-[55px] hover:scale-[1.03] hover:rotate-2  transition-all duration-500 p-14">
                     <video
                         src={src}
                         autoPlay
@@ -19,13 +19,13 @@ export default function ContentSection({ text, src, order }: ContentSectionProps
                         loop
                         playsInline
                         preload="metadata"
-                        className="w-full h-full object-contain rounded-[60px]"
+                        className="w-full h-full object-contain rounded-[55px]"
                     />
                 </div>
             </div>
         ) : (
             <div className="flex items-center justify-between my-20 lg:flex-row flex-col-reverse">
-                <div className="relative group max-w-sm sm:max-w-md  rounded-[60px] hover:scale-[1.03] hover:-rotate-2  transition-all duration-500 p-14">
+                <div className="relative group max-w-sm sm:max-w-md  rounded-[55px] hover:scale-[1.03] hover:-rotate-2  transition-all duration-500 p-14">
                     <video
                         src={src}
                         autoPlay
@@ -33,7 +33,7 @@ export default function ContentSection({ text, src, order }: ContentSectionProps
                         loop
                         playsInline
                         preload="metadata"
-                        className="w-full h-full object-contain rounded-[60px]"
+                        className="w-full h-full object-contain rounded-[55px]"
                     />
                 </div>
                 <div className="text-center px-4 my-20 lg:my-0">
