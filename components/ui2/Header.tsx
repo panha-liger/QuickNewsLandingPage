66c6c8aad496d0db2a235e72c11bac278c8d542b
Chanraksa Ory <OCraksa@gmail.com>
2025-10-24 13:36:25 +0700

    fix: changed navbar

--- components/ui2/Header.tsx
+++ components/ui2/Header.tsx
@@ -1,12 +1,12 @@
 import Link from "next/link";
 import Logo from "../ui/logo";
 
-const NavLink = ({ href, children, isButton = false }: { href: string; children: React.ReactNode; isButton?: boolean }) => {
+const NavLink = ({ href, children, isButton = false, hiddenOnMobile = false }: { href: string; children: React.ReactNode; isButton?: boolean; hiddenOnMobile?: boolean }) => {
     if (isButton) {
         return (
             <Link
                 href={href}
-                className="inline-flex h-10 px-4 py-2 bg-black text-white font-bold text-lg transition-all rounded-full hover:bg-gray-800"
+                className={`inline-flex h-10 px-4 py-2 bg-black text-white font-bold text-lg transition-all rounded-full hover:bg-gray-800 ${hiddenOnMobile ? "hidden sm:inline-flex" : ""}`}
             >
                 {children}
             </Link>
@@ -16,11 +16,11 @@ const NavLink = ({ href, children, isButton = false }: { href: string; children:
     return (
         <Link
             href={href}
-            className="relative inline-flex h-10 px-4 py-2 text-lg font-bold  transition-colors group"
+            className={`relative inline-flex h-10 px-4 py-2 text-lg font-bold  transition-colors group ${hiddenOnMobile ? "hidden sm:inline-flex" : ""}`}
         >
             <span className="relative">
                 {children}
-                <span className="absolute -bottom-3 left-1/2 w-0 h-0.5 bg-gray-500 transition-all duration-500 ease-out group-hover:w-full group-hover:left-0"></span>
+                <span className="absolute -bottom-2 left-1/2 w-0 h-0.5 bg-gray-500 transition-all duration-500 ease-out group-hover:w-full group-hover:left-0"></span>
             </span>
         </Link>
     );
@@ -39,11 +39,11 @@ export default function Header() {
                     </div> */}
 
                     {/* Nav items - wrap on small screens */}
-                    <div className="flex flex-wrap items-center justify-center gap-2 sm:gap-2 w-full sm:w-auto  sm:justify-end">
-                        <NavLink href="/creators">Content Creator</NavLink>
-                        <NavLink href="/advertisers">Advertiser</NavLink>
-                        <NavLink href="#faq">What is Quicknews?</NavLink>
-                        <NavLink href="#waitlist" isButton>Win $100</NavLink>
+                    <div className="flex flex-wrap items-center justify-center gap-2 sm:gap-2 w-full sm:w-auto">
+                        <NavLink href="/creators" >I&apos;m a Content Creator</NavLink>
+                        <NavLink href="/advertisers" >I&apos;m an Advertiser</NavLink>
+                        <NavLink href="#faq" hiddenOnMobile>What is Quicknews?</NavLink>
+                        <NavLink href="#waitlist" isButton hiddenOnMobile>Win $100</NavLink>
                     </div>
                 </div>
             </div>
